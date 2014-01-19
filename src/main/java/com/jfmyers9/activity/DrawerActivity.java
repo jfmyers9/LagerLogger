2bf2a87f857e4f60dc78c900ba37ddd46fdd3fc0
James Myers <jfmyers9@gmail.com>
2014-01-18 21:31:38 -0800

    Add open/close drawer functionality. Drawer populates with a string array of locations available

--- src/main/java/com/jfmyers9/activity/DrawerActivity.java
+++ src/main/java/com/jfmyers9/activity/DrawerActivity.java
@@ -1,17 +1,69 @@
 package com.jfmyers9.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.jfmyers9.R;
 
 import roboguice.activity.RoboFragmentActivity;
 
 public class DrawerActivity extends RoboFragmentActivity {
+    private String[] sidebarItems;
+    private DrawerLayout drawerLayout;
+    private ListView lv;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.app_name);
         setContentView(R.layout.drawer_layout);
+
+        sidebarItems = getResources().getStringArray(R.array.sidebar_items);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.app_name, R.string.app_name) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        lv = (ListView) findViewById(R.id.left_drawer);
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, sidebarItems));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
