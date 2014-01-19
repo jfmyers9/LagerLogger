b5f8c2c219c5a74e931ad309393c627e686b84b4
James Myers <jfmyers9@gmail.com>
2014-01-18 20:44:38 -0800

    Reformat the code and perform a useful test

--- src/main/java/com/jfmyers9/activity/DrawerActivity.java
+++ src/main/java/com/jfmyers9/activity/DrawerActivity.java
@@ -1,15 +1,17 @@
 package com.jfmyers9.activity;
 
 import android.os.Bundle;
+
 import com.jfmyers9.R;
 
 import roboguice.activity.RoboFragmentActivity;
 
 public class DrawerActivity extends RoboFragmentActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.drawer_layout);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name);
+        setContentView(R.layout.drawer_layout);
+    }
 }
--- src/test/java/com/jfmyers9/activity/DrawerActivityTest.java
+++ src/test/java/com/jfmyers9/activity/DrawerActivityTest.java
@@ -1,6 +1,8 @@
 package com.jfmyers9.activity;
 
 import android.app.Activity;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
@@ -10,9 +12,9 @@ import static org.junit.Assert.assertTrue;
 @RunWith(RobolectricTestRunner.class)
 public class DrawerActivityTest {
 
-  @org.junit.Test
-  public void testSomething() throws Exception {
-    Activity activity = Robolectric.buildActivity(DrawerActivity.class).create().get();
-    assertTrue(activity != null);
-  }
+    @Test
+    public void testThatTheTitleIsCorrect() throws Exception {
+        Activity activity = Robolectric.buildActivity(DrawerActivity.class).create().get();
+        assertTrue(activity.getTitle().equals("Lager Logger"));
+    }
 }
